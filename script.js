dff94078597095cb01fdf5351d0c05dece3bd3e4
Mateusz Świstak <swistakmatt@gmail.com>
2025-05-22 00:30:22 +0200

    LAB3 tasks done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let clickCount = 0;
 
 const countryInput = document.getElementById('country');
+const countryList = document.getElementById('country-list');
 const myForm = document.getElementById('form');
 const modal = document.getElementById('form-feedback-modal');
 const clicksInfo = document.getElementById('click-count');
@@ -17,9 +18,9 @@ async function fetchAndFillCountries() {
 			throw new Error('Błąd pobierania danych');
 		}
 		const data = await response.json();
-		const countries = data.map((country) => country.name.common);
-		countryInput.innerHTML = countries
-			.map((country) => `<option value="${country}">${country}</option>`)
+		const countries = data.map((country) => country.name.common).sort();
+		countryList.innerHTML = countries
+			.map((c) => `<option value="${c}">`)
 			.join('');
 	} catch (error) {
 		console.error('Wystąpił błąd:', error);
@@ -31,7 +32,8 @@ function getCountryByIP() {
 		.then((response) => response.json())
 		.then((data) => {
 			const country = data.country;
-			// TODO inject country to form and call getCountryCode(country) function
+			countryInput.value = country;
+			getCountryCode(country);
 		})
 		.catch((error) => {
 			console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -50,7 +52,10 @@ function getCountryCode(countryName) {
 		})
 		.then((data) => {
 			const countryCode = data[0].idd.root + data[0].idd.suffixes.join('');
-			// TODO inject countryCode to form
+			const radios = document.getElementsByName('countryCode');
+			radios.forEach((r) => {
+				if (r.value === countryCode) r.checked = true;
+			});
 		})
 		.catch((error) => {
 			console.error('Wystąpił błąd:', error);
@@ -62,4 +67,26 @@ function getCountryCode(countryName) {
 	document.addEventListener('click', handleClick);
 
 	fetchAndFillCountries();
+	getCountryByIP();
+
+	myForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const bsModal = new bootstrap.Modal(modal);
+		clicksInfo.innerText = clickCount;
+		bsModal.show();
+	});
+	document.addEventListener('keydown', (e) => {
+		if (e.key === 'Enter' && document.activeElement.tagName !== 'TEXTAREA') {
+			e.preventDefault();
+			myForm.requestSubmit();
+		}
+
+		if (
+			(e.ctrlKey && e.key.toLowerCase() === 's') ||
+			(e.ctrlKey && e.key === 'Enter')
+		) {
+			e.preventDefault();
+			myForm.requestSubmit();
+		}
+	});
 })();
